cccdb6abc1d414ec5856a2b1be1be08a387202b7
shuleihen <shuleihen@126.com>
2016-07-28 18:00:35 +0800

    文章详情页面详情保存sharpModel对象中

---
+++ TDjuwairen/SharpDesc/Models/SharpModel.h
@@ -0,0 +1,13 @@
+//
+//  SharpModel.h
+//  TDjuwairen
+//
+//  Created by zdy on 16/7/28.
+//  Copyright © 2016年 团大网络科技. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SharpModel : NSObject
+
+@end
---
+++ TDjuwairen/SharpDesc/Models/SharpModel.m
@@ -0,0 +1,13 @@
+//
+//  SharpModel.m
+//  TDjuwairen
+//
+//  Created by zdy on 16/7/28.
+//  Copyright © 2016年 团大网络科技. All rights reserved.
+//
+
+#import "SharpModel.h"
+
+@implementation SharpModel
+
+@end
