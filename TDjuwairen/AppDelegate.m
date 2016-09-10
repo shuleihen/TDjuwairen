a0f8d783731101c56017eef5d69ecdcf77a0895f
RylaiCrestfall <450924349@qq.com>
2016-09-10 16:36:03 +0800

    替换极光推送为百度推送（无奈- -）

--- TDjuwairen/AppDelegate.m
+++ TDjuwairen/AppDelegate.m
@@ -25,6 +25,9 @@
 #import "UIdaynightModel.h"
 #import "CocoaLumberjack.h"
 
+#import "BPush.h"
+
+static BOOL isBackGroundActivateApplication;
 @interface AppDelegate ()
 @end
 
@@ -40,6 +43,7 @@
     [self setupWebImageCache];
     [self checkSwitchToGuide];
     [self setupLog];
+    [self setupWithBaiDuPushWithDic:launchOptions];
     
     return YES;
 }
@@ -108,7 +112,7 @@
     }
     
     
- 
+    
     [UITabBar appearance].barTintColor = daynightmodel.navigationColor;
     [UITabBar appearance].tintColor = [HXColor hx_colorWithHexRGBAString:@"#1b69b1"];
     [UITabBar appearance].translucent = NO;
@@ -237,10 +241,135 @@
     fileLogger.logFileManager.maximumNumberOfLogFiles = 7;
     [DDLog addLogger:fileLogger];
     
-//    DDLogVerbose(@"Verbose");
-//    DDLogDebug(@"Debug");
-//    DDLogInfo(@"Info");
-//    DDLogWarn(@"Warn");
-//    DDLogError(@"Error");
+    //    DDLogVerbose(@"Verbose");
+    //    DDLogDebug(@"Debug");
+    //    DDLogInfo(@"Info");
+    //    DDLogWarn(@"Warn");
+    //    DDLogError(@"Error");
+}
+
+#pragma mark - 百度推送
+- (void)setupWithBaiDuPushWithDic:(NSDictionary *)launchOptions{
+    // 在 App 启动时注册百度云推送服务，需要提供 Apikey
+    [BPush registerChannel:launchOptions apiKey:@"YewcrZIsfLIvO2MNoOXIO8ru" pushMode:BPushModeDevelopment withFirstAction:@"打开" withSecondAction:@"回复" withCategory:@"test" useBehaviorTextInput:YES isDebug:YES];
+    // App 是用户点击推送消息启动
+    NSDictionary *userInfo = [launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
+    if (userInfo) {
+        NSLog(@"从消息启动:%@",userInfo);
+        [BPush handleNotification:userInfo];
+    }
+    //角标清0
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
+    [[UIApplication sharedApplication] cancelAllLocalNotifications];
+    
+    /*
+     // 测试本地通知
+     [self performSelector:@selector(testLocalNotifi) withObject:nil afterDelay:1.0];
+     */
+}
+
+- (void)testLocalNotifi
+{
+    NSLog(@"测试本地通知啦！！！");
+    NSDate *fireDate = [[NSDate new] dateByAddingTimeInterval:5];
+    [BPush localNotification:fireDate alertBody:@"这是本地通知" badge:3 withFirstAction:@"打开" withSecondAction:@"关闭" userInfo:nil soundName:nil region:nil regionTriggersOnce:YES category:nil useBehaviorTextInput:YES];
+}
+
+// 此方法是 用户点击了通知，应用在前台 或者开启后台并且应用在后台 时调起
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+    // 打印到日志 textView 中
+    
+    NSLog(@"********** iOS7.0之后 background **********");
+    //杀死状态下，直接跳转到跳转页面。
+    if (application.applicationState == UIApplicationStateInactive && !isBackGroundActivateApplication)
+    {
+        //        SkipViewController *skipCtr = [[SkipViewController alloc]init];
+        //        // 根视图是nav 用push 方式跳转
+        //        [_tabBarCtr.selectedViewController pushViewController:skipCtr animated:YES];
+        NSLog(@"applacation is unactive ===== %@",userInfo);
+        /*
+         // 根视图是普通的viewctr 用present跳转
+         [_tabBarCtr.selectedViewController presentViewController:skipCtr animated:YES completion:nil]; */
+    }
+    // 应用在后台。当后台设置aps字段里的 content-available 值为 1 并开启远程通知激活应用的选项
+    if (application.applicationState == UIApplicationStateBackground) {
+        NSLog(@"background is Activated Application ");
+        // 此处可以选择激活应用提前下载邮件图片等内容。
+        isBackGroundActivateApplication = YES;
+        UIAlertView *alertView =[[UIAlertView alloc]initWithTitle:@"收到一条消息" message:userInfo[@"aps"][@"alert"] delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
+        [alertView show];
+    }
+    
+    completionHandler(UIBackgroundFetchResultNewData);
+    NSLog(@"backgroud : %@",userInfo);
+    
+}
+
+// 在 iOS8 系统中，还需要添加这个方法。通过新的 API 注册推送服务
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
+{
+    
+    [application registerForRemoteNotifications];
+    
+    
+}
+
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    NSLog(@"test:%@",deviceToken);
+    [BPush registerDeviceToken:deviceToken];
+    [BPush bindChannelWithCompleteHandler:^(id result, NSError *error) {
+        // 需要在绑定成功后进行 settag listtag deletetag unbind 操作否则会失败
+        // 网络错误
+        if (error) {
+            return ;
+        }
+        if (result) {
+            // 确认绑定成功
+            if ([result[@"error_code"]intValue]!=0) {
+                return;
+            }
+            [BPush setTag:@"Mytag" withCompleteHandler:^(id result, NSError *error) {
+                if (result) {
+                    NSLog(@"设置tag成功");
+                }
+            }];
+        }
+    }];
+    
+    
+}
+
+// 当 DeviceToken 获取失败时，系统会回调此方法
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    NSLog(@"DeviceToken 获取失败，原因：%@",error);
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    // App 收到推送的通知
+    [BPush handleNotification:userInfo];
+    NSLog(@"********** ios7.0之前 **********");
+    // 应用在前台 或者后台开启状态下，不跳转页面，让用户选择。
+    if (application.applicationState == UIApplicationStateActive || application.applicationState == UIApplicationStateBackground) {
+        NSLog(@"acitve or background");
+        UIAlertView *alertView =[[UIAlertView alloc]initWithTitle:@"收到一条消息" message:userInfo[@"aps"][@"alert"] delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
+        [alertView show];
+    }
+    else//杀死状态下，直接跳转到跳转页面。
+    {
+        
+    }
+    NSLog(@"%@",userInfo);
+    
+}
+
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
+{
+    NSLog(@"接收本地通知啦！！！");
+    [BPush showLocalNotificationAtFront:notification identifierKey:nil];
 }
 @end
