09fd2cacdb52e3981cb3d54448a2823cccad3cd8
RylaiCrestfall <450924349@qq.com>
2016-12-19 09:59:58 +0800

    猜红绿

---
+++ TDjuwairen/Modules/TDGame/Controllers/GameSelectViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameSelectViewController.h
+//  TDjuwairen
+//
+//  Created by 团大 on 2016/12/19.
+//  Copyright © 2016年 团大网络科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameSelectViewController : UIViewController
+
+@end
---
+++ TDjuwairen/Modules/TDGame/Controllers/GuessRedGreenViewController.m
@@ -0,0 +1,37 @@
+//
+//  GuessRedGreenViewController.m
+//  TDjuwairen
+//
+//  Created by 团大 on 2016/12/19.
+//  Copyright © 2016年 团大网络科技. All rights reserved.
+//
+
+#import "GuessRedGreenViewController.h"
+
+@interface GuessRedGreenViewController ()
+
+@end
+
+@implementation GuessRedGreenViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
