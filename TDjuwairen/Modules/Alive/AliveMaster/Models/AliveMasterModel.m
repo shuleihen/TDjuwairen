a0631fc348449c6b1549da1ea245313572137d44
Floater_ping <pingping_float@163.com>
2017-03-10 11:25:57 +0800

    test01

--- TDjuwairen/Modules/Alive/AliveMaster/Models/AliveMasterModel.m
+++ TDjuwairen/Modules/Alive/AliveMaster/Models/AliveMasterModel.m
@@ -6,7 +6,7 @@
 //  Copyright © 2017年 团大网络科技. All rights reserved.
 //
 
-#import "AliveMasterListModel.h"
+#import "AliveMasterModel.h"
 
 @implementation AliveMasterModel
 - (id)initWithDictionary:(NSDictionary *)dict {
