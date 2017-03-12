44b532f4cf49966ed49e0ac20217b024e43e9c87
Floater_ping <pingping_float@163.com>
2017-03-12 16:42:41 +0800

    test

--- TDjuwairen/Modules/Alive/AliveDetail/Controllers/AliveDetailViewController.m
+++ TDjuwairen/Modules/Alive/AliveDetail/Controllers/AliveDetailViewController.m
@@ -9,6 +9,7 @@
 #import "AliveDetailViewController.h"
 
 @interface AliveDetailViewController ()
+@property (nonatomic, strong) UITableView *tableView;
 
 @end
 
@@ -16,22 +17,81 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.title = @"直播正文";
+    [self setUpValue];
+    [self setUpUICommon];
+   
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (UITableView *)tableView {
+    if (!_tableView) {
+        CGRect rect = CGRectMake(0, 0, kScreenWidth, kScreenHeight-64-50);
+        _tableView = [[UITableView alloc] initWithFrame:rect style:UITableViewStyleGrouped];
+        _tableView.backgroundColor = TDViewBackgrouondColor;
+        _tableView.separatorColor = TDSeparatorColor;
+        _tableView.separatorInset = UIEdgeInsetsZero;
+        _tableView.showsVerticalScrollIndicator = NO;
+        
+    }
+    
+    return _tableView;
 }
-*/
+
+
+- (void)setUpValue {
+    
+    
+}
+
+- (void)setUpUICommon {
+    
+}
+
+
+
+
+
+#pragma mark -------------- UITableViewDataSource ---------------------
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    return nil;
+}
+
+#pragma mark ----------------------------------------------------------
+
+
+
+#pragma mark -------------- UITableViewDelegate ---------------------
+//- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+//[tableView deselectRowAtIndexPath:indexPath animated:YES];
+//}
+
+//- (nullable UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section;
+
+//- (nullable UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section;
+
+//- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath;
+
+//- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section;
+
+//- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section;
+
+#pragma mark ----------------------------------------------------------
 
 @end
