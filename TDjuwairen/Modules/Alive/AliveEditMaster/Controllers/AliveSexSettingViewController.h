1485a509fdd0c9ba25080d830d6bd3790b112984
shuleihen <shuleihen@126.com>
2017-03-13 09:56:50 +0800

    股票详情评级入口调整凸显

---
+++ TDjuwairen/Modules/Alive/AliveEditMaster/Controllers/AliveSexSettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  AliveSexSettingViewController.h
+//  TDjuwairen
+//
+//  Created by zdy on 2017/3/12.
+//  Copyright © 2017年 团大网络科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AliveSexSettingViewController : UITableViewController
+
+@end
