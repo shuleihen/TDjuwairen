6acb2403f2494512f4b3ff4f9535e3ce80ee8a07
deng shu <dengshu@dengdeMacBook-Pro.local>
2017-03-30 15:04:48 +0800

    个股竞猜评论

--- TDjuwairen/Modules/PlayStock/PlayIndividualStock/Controllers/PlayIndividualStockContentViewController.m
+++ TDjuwairen/Modules/PlayStock/PlayIndividualStock/Controllers/PlayIndividualStockContentViewController.m
@@ -18,6 +18,7 @@
 #import "NetworkManager.h"
 #import "MBProgressHUD.h"
 #import "PlayGuessIndividua.h"
+#import "CommentsViewController.h"
 
 @interface PlayIndividualStockContentViewController ()<UITableViewDelegate,UITableViewDataSource,GuessAddPourDelegate>
 @property (nonatomic, strong) UITableView *tableView;
@@ -69,17 +70,17 @@ static NSString *KPlayIndividualContentCell = @"PlayIndividualContentCell";
 #pragma mark -UITableViewDataSource
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
 
-    return _listArr.count;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 1;
+    return _listArr.count;
     
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
 
-    PlayListModel *model = _listArr[indexPath.section];
+    PlayListModel *model = _listArr[indexPath.row];
     PlayIndividualContentCell *cell = [PlayIndividualContentCell loadCell];
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
     cell.model = model;
@@ -183,6 +184,55 @@ static NSString *KPlayIndividualContentCell = @"PlayIndividualContentCell";
 {
     return 141;
 }
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return 44.0f;
+}
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
+    
+    
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, 44)];
+    view.backgroundColor = [UIColor hx_colorWithHexRGBAString:@"#101115"];
+    
+    
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = view.bounds;
+    btn.titleLabel.font = [UIFont systemFontOfSize:15.0f];
+    
+    [btn setTitleColor:[UIColor hx_colorWithHexRGBAString:@"#666666"] forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor hx_colorWithHexRGBAString:@"#666666"] forState:UIControlStateHighlighted];
+    [btn addTarget:self action:@selector(commentPressed:) forControlEvents:UIControlEventTouchUpInside];
+    
+    if (self.guessModel.guess_comment_count > 0) {
+        NSString *title = [NSString stringWithFormat:@"评价(%@)",self.guessModel.guess_comment_count];
+        [btn setTitle:title forState:UIControlStateNormal];
+        [btn setTitle:title forState:UIControlStateHighlighted];
+    } else {
+        [btn setTitle:@"评价" forState:UIControlStateNormal];
+        [btn setTitle:@"评价" forState:UIControlStateHighlighted];
+    }
+    
+    [view addSubview:btn];
+    return view;
+}
+
+- (void)commentPressed:(UIButton *)sender{
+    if (US.isLogIn==NO) {//检查是否登录，没有登录直接跳转登录界面
+        //跳转到登录页面
+        LoginViewController *login = [[LoginViewController alloc] init];
+        login.hidesBottomBarWhenPushed = YES;//跳转时隐藏tabbar
+        [self.superVC.navigationController pushViewController:login animated:YES];
+    }
+    else//登录后 跳转评论管理页面
+    {
+        CommentsViewController *comments = [[CommentsViewController alloc] init];
+        comments.hidesBottomBarWhenPushed = YES;
+        [self.superVC.navigationController pushViewController:comments animated:YES];
+    }
+}
+
 #pragma mark -
 
 
