76bebb2993400b91a8556acc405ac9137ca7df88
pingping_float@163.com <pingping_float@163.com>
2017-08-10 17:32:20 +0800

    下班提交代码

--- TDjuwairen/Modules/StockPool/Views/StockPoolRecordBottomView.m
+++ TDjuwairen/Modules/StockPool/Views/StockPoolRecordBottomView.m
@@ -27,12 +27,43 @@
     if (self = [super init]) {
         self.backgroundColor = [UIColor redColor];
         _settingBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-        [_settingBtn setImage:[UIImage imageNamed:@""] forState:UIControlStateNormal];
+        [_settingBtn setImage:[UIImage imageNamed:@"ico_setting"] forState:UIControlStateNormal];
+        [_settingBtn addTarget:self action:@selector(settingBtnClick) forControlEvents:UIControlEventTouchUpInside];
+        _settingBtn.frame = CGRectMake(22, 14.5, 22, 20);
         [self addSubview:_settingBtn];
         
+        _draftBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_draftBtn setImage:[UIImage imageNamed:@"ico_drafts"] forState:UIControlStateNormal];
+        [_draftBtn addTarget:self action:@selector(draftBtnClick) forControlEvents:UIControlEventTouchUpInside];
+        _draftBtn.frame = CGRectMake(70, 14.5, 22, 20);
+        [self addSubview:_draftBtn];
         
+        _bookKeepingBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_bookKeepingBtn setImage:[UIImage imageNamed:@"ico_drafts"] forState:UIControlStateNormal];
+        [_bookKeepingBtn addTarget:self action:@selector(bookKeepingBtnClick) forControlEvents:UIControlEventTouchUpInside];
+        _bookKeepingBtn.frame = CGRectMake(70, 14.5, 22, 20);
+        [self addSubview:_bookKeepingBtn];
     }
     return self;
 }
+
+#pragma - actions
+/** 设置按钮点击事件 */
+- (void)settingBtnClick {
+
+    
+}
+
+/** 草稿箱按钮点击事件 */
+- (void)draftBtnClick {
+    
+}
+
+/** 记账按钮点击事件 */
+- (void)bookKeepingBtnClick {
+    
+}
+
+
 
 @end
