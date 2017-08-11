999245a433d8e478e57aa4fe3201722018c791a9
pingping_float@163.com <pingping_float@163.com>
2017-08-11 09:14:13 +0800

    添加股票池设置类和入口

--- TDjuwairen/Modules/StockPool/StockPoolList/Controllers/StockPoolListViewController.m
+++ TDjuwairen/Modules/StockPool/StockPoolList/Controllers/StockPoolListViewController.m
@@ -11,6 +11,7 @@
 #import "StockPoolListCell.h"
 #import "StockPoolListToolView.h"
 #import "LoginStateManager.h"
+#import "StockPoolSettingController.h"
 
 
 @interface StockPoolListViewController ()<UITableViewDelegate, UITableViewDataSource, StockPoolListToolViewDelegate>
@@ -114,7 +115,8 @@
 
 #pragma mark - StockPoolListToolViewDelegate
 - (void)settingPressed:(id)sender {
-    
+    StockPoolSettingController *settingVC = [[StockPoolSettingController alloc] init];
+    [self.navigationController pushViewController:settingVC animated:YES];
 }
 
 - (void)draftPressed:(id)sender {
---
+++ TDjuwairen/Modules/StockPool/StockPoolSetting/Controllers/StockPoolSettingController.m
@@ -0,0 +1,30 @@
+//
+//  StockPoolSettingController.m
+//  TDjuwairen
+//
+//  Created by ZYP-MAC on 2017/8/11.
+//  Copyright © 2017年 团大网络科技. All rights reserved.
+//
+
+#import "StockPoolSettingController.h"
+
+@interface StockPoolSettingController ()
+
+@end
+
+@implementation StockPoolSettingController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"设置";
+    self.view.backgroundColor = TDViewBackgrouondColor;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+   
+}
+
+
+
+@end
