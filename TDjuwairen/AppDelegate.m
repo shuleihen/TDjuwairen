074a3475e002685754187a4dcf639468c6128c49
shuleihen <shuleihen@126.com>
2017-08-11 10:12:36 +0800

    微信sdk替换为没有支付功能的

--- TDjuwairen/AppDelegate.m
+++ TDjuwairen/AppDelegate.m
@@ -116,102 +116,13 @@
   sourceApplication:(NSString *)sourceApplication
          annotation:(id)annotation
 {
-    /*
-    if ([url.host isEqualToString:@"safepay"]) {
-        // 支付跳转支付宝钱包进行支付，处理支付结果
-        [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
-            NSLog(@"result = %@",resultDic);
-            NSString *resultStatus = resultDic[@"resultStatus"];
-            if ([resultStatus isEqualToString:@"9000"]) {
-                //通知支付成功
-                [[NSNotificationCenter defaultCenter] postNotificationName:@"paySuccess" object:nil];
-            }
-        }];
-        
-        // 授权跳转支付宝钱包进行支付，处理支付结果
-        [[AlipaySDK defaultService] processAuth_V2Result:url standbyCallback:^(NSDictionary *resultDic) {
-            NSLog(@"result = %@",resultDic);
-            // 解析 auth code
-            NSString *result = resultDic[@"result"];
-            NSString *authCode = nil;
-            if (result.length>0) {
-                NSArray *resultArr = [result componentsSeparatedByString:@"&"];
-                for (NSString *subResult in resultArr) {
-                    if (subResult.length > 10 && [subResult hasPrefix:@"auth_code="]) {
-                        authCode = [subResult substringFromIndex:10];
-                        break;
-                    }
-                }
-            }
-            NSLog(@"授权结果 authCode = %@", authCode?:@"");
-        }];
-        
-        [[AlipaySDK defaultService] processAuthResult:url standbyCallback:^(NSDictionary *resultDic) {
-            NSLog(@"%@",resultDic);
-            //通知刷新钥匙
-            [[NSNotificationCenter defaultCenter] postNotificationName:@"refreshKeys" object:nil];
-        }];
-        return YES;
-    }
-    else
-    {
-        return [WXApi handleOpenURL:url delegate:[WXApiManager sharedManager]];
-    }
-    */
-    
     return [WXApi handleOpenURL:url delegate:[WXApiManager sharedManager]];
-    
 }
 
 // NOTE: 9.0以后使用新API接口
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString*, id> *)options
 {
-    /*
-    if ([url.host isEqualToString:@"safepay"]) {
-        // 支付跳转支付宝钱包进行支付，处理支付结果
-        [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
-            NSLog(@"result = %@",resultDic);
-            NSString *resultStatus = resultDic[@"resultStatus"];
-            if ([resultStatus isEqualToString:@"9000"]) {
-                //通知支付成功
-                [[NSNotificationCenter defaultCenter] postNotificationName:@"paySuccess" object:nil];
-            }
-        }];
-        
-        // 授权跳转支付宝钱包进行支付，处理支付结果
-        [[AlipaySDK defaultService] processAuth_V2Result:url standbyCallback:^(NSDictionary *resultDic) {
-            NSLog(@"result = %@",resultDic);
-            // 解析 auth code
-            NSString *result = resultDic[@"result"];
-            NSString *authCode = nil;
-            if (result.length>0) {
-                NSArray *resultArr = [result componentsSeparatedByString:@"&"];
-                for (NSString *subResult in resultArr) {
-                    if (subResult.length > 10 && [subResult hasPrefix:@"auth_code="]) {
-                        authCode = [subResult substringFromIndex:10];
-                        break;
-                    }
-                }
-            }
-            NSLog(@"授权结果 authCode = %@", authCode?:@"");
-        }];
-        
-        [[AlipaySDK defaultService] processAuthResult:url standbyCallback:^(NSDictionary *resultDic) {
-            NSLog(@"%@",resultDic);
-            //通知刷新钥匙
-            [[NSNotificationCenter defaultCenter] postNotificationName:@"refreshKeys" object:nil];
-            
-            
-        }];
-        return YES;
-    }
-    else
-    {
-        return [WXApi handleOpenURL:url delegate:[WXApiManager sharedManager]];
-    }
-     */
     return [WXApi handleOpenURL:url delegate:[WXApiManager sharedManager]];
-    
 }
 
 - (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url
--- TDjuwairen/Modules/Recharge/Controllers/RechargeChooseViewController.m
+++ TDjuwairen/Modules/Recharge/Controllers/RechargeChooseViewController.m
@@ -10,7 +10,6 @@
 #import "NetworkManager.h"
 #import "LoginStateManager.h"
 #import "STPopup.h"
-#import "WXApi.h"
 
 @interface RechargeChooseViewController ()
 @property (weak, nonatomic) IBOutlet UIView *contentView;
@@ -26,72 +25,11 @@
 }
 
 - (IBAction)zhifbaoPressed:(id)sender {
-    /*
-    NSDictionary *dic = @{@"type":    @(self.type),
-                          @"number":  @(self.keyNumber),
-                          @"version": @"1.0",
-                          @"user_id": US.userId};
-    
 
-    NetworkManager *ma = [[NetworkManager alloc] init];
-    [ma POST:API_AliPay parameters:dic completion:^(id data, NSError *error){
-        if (!error && data) {
-            if ([data isKindOfClass:[NSString class]]) {
-                NSString *orderString = data;
-                NSString *appScheme = @"TDjuwairen";
-                
-                [[AlipaySDK defaultService] payOrder:orderString fromScheme:appScheme callback:^(NSDictionary *resultDic) {
-                    //                DDLogInfo(@"reslut = %@",resultDic);
-                    
-                }];
-                
-                
-            } else {
-                
-            }
-        } else {
-            
-        }
-        
-        [self.popupController dismiss];
-    }];
-     */
 }
 
 - (IBAction)wexinPressed:(id)sender {
-    NSDictionary *dic = @{@"type":    @(self.type),
-                          @"number":  @(self.keyNumber),
-                          @"device": @"1",
-                          @"user_id": US.userId};
-    
-    NetworkManager *ma = [[NetworkManager alloc] init];
-    [ma POST:API_WXPay parameters:dic completion:^(id data, NSError *error){
-        if (!error && data) {
-            
-            NSData *jsonData = [data dataUsingEncoding:NSUTF8StringEncoding];
-            NSError *err;
-            NSDictionary *order = [NSJSONSerialization JSONObjectWithData:jsonData
-                                                                  options:NSJSONReadingMutableContainers
-                                                                    error:&err];
-            if (!error) {
-                //调起微信支付
-                PayReq* req             = [[PayReq alloc] init];
-                req.openID              = [order objectForKey:@"appid"];
-                req.partnerId           = [order objectForKey:@"mch_id"];
-                req.prepayId            = [order objectForKey:@"prepay_id"];
-                req.nonceStr            = [order objectForKey:@"nonce_str"];
-                req.timeStamp           = [[order objectForKey:@"timestamp"] intValue];
-                req.package             = @"Sign=WXPay";
-                req.sign                = [order objectForKey:@"sign"];
-                
-                [WXApi sendReq:req];
-            } else {
-                
-            }
-        }
-        
-        [self.popupController dismiss];
-    }];
+ 
 }
 
 @end
